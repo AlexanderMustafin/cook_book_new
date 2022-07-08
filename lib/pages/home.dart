58ddab98fb617d2b7ae9b051fce6159bc08232c6
Alexmiln <alexmiln04@gmail.com>
2022-07-08 13:22:57 +0300

    Изменил внешний вид разделов на главном экране

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:cook_book_new/pages/salads.dart';
 import 'package:cook_book_new/pages/DarkThemePreference.dart';
 import 'package:provider/provider.dart';
+// import 'package:cook_book_new/resources/theme.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -30,73 +31,311 @@ class _HomeState extends State<Home> {
         ),
         ListTile(
           leading: Icon(Icons.favorite_border),
-          title: Text('Favorite'),
+          title: Text('Favorites'),
           onTap: () {},
         ),
         ListTile(
           leading: Icon(Icons.shopping_bag_outlined),
-          title: Text('Shopping basket'),
+          title: Text('Shopping cart'),
           onTap: () {},
         ),
         SwitchListTile(
           title: const Text('Dark mode'),
-            value: themeChange.darkTheme,
-            onChanged: (value) {
-              setState(() {
-                themeChange.darkTheme = value;
-              });
-            },
-            secondary: Icon(Icons.dark_mode_outlined)
-            ),
+          value: themeChange.darkTheme,
+          onChanged: (value) {
+            setState(() {
+              themeChange.darkTheme = value;
+            });
+        },
+            secondary: const Icon(Icons.dark_mode_outlined),
+        ),
+
       ])),
       appBar: AppBar(
         backgroundColor: Colors.amber,
-        title: Text('Cook book'),
+        title: Text('Cookbook'),
         centerTitle: true,
       ),
-      body: ListView(
-        children: <Widget>[
-          ListTile(
-            leading:
-                Image.asset("assets/img/salad.jpg", width: 70, height: 100),
-            title: Text('Salads'),
-            subtitle: Text('Кол-во предложений'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => Salads()),
-              );
-            },
+    body: GridView.count(
+      primary: false,
+      padding: const EdgeInsets.all(20),
+      crossAxisSpacing: 10,
+      mainAxisSpacing: 10,
+      crossAxisCount: 2,
+      children: <Widget>[
+        InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Salads()),
+            );
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage(
+                    'assets/img/salad.jpg'),
+                fit: BoxFit.fitHeight,
+              ),
+              borderRadius: BorderRadius.circular(16),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade600,
+                  spreadRadius: 1,
+                  blurRadius: 5,
+                  offset: const Offset(0, 5),
+                )
+              ],
+            ),
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const [
+                Text(
+                  'Salads',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
           ),
-          ListTile(
-            leading:
-                Image.asset("assets/img/snacks.jpg", width: 70, height: 100),
-            title: Text('Snacks, sandwiches'),
-            subtitle: Text('Кол-во предложений'),
-            onTap: () {},
+        ),
+
+        InkWell(
+          // onTap: () {
+          //   Navigator.push(
+          //     context,
+          //     MaterialPageRoute(builder: (context) => Salads()),
+          //   );
+          // },
+          child: Container(
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage(
+                    'assets/img/snacks.jpg'),
+                fit: BoxFit.fitHeight,
+              ),
+              borderRadius: BorderRadius.circular(16),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade600,
+                  spreadRadius: 1,
+                  blurRadius: 5,
+                  offset: const Offset(0, 5),
+                )
+              ],
+            ),
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const [
+                Text(
+                  'Snacks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
           ),
-          ListTile(
-            leading: Image.asset("assets/img/soup.jpg", width: 70, height: 100),
-            title: Text('First meal'),
-            subtitle: Text('Кол-во предложений'),
-            onTap: () {},
+        ),
+
+        InkWell(
+          // onTap: () {
+          //   Navigator.push(
+          //     context,
+          //     MaterialPageRoute(builder: (context) => Salads()),
+          //   );
+          // },
+          child: Container(
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage(
+                    'assets/img/soup.jpg'),
+                fit: BoxFit.fitHeight,
+              ),
+              borderRadius: BorderRadius.circular(16),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade600,
+                  spreadRadius: 1,
+                  blurRadius: 5,
+                  offset: const Offset(0, 5),
+                )
+              ],
+            ),
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const [
+                Text(
+                  'First Meal',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
           ),
-          ListTile(
-            leading: Image.asset("assets/img/second_meal.jpg",
-                width: 70, height: 100),
-            title: Text('Second meal'),
-            subtitle: Text('Кол-во предложений'),
-            onTap: () {},
+        ),
+
+
+        InkWell(
+          // onTap: () {
+          //   Navigator.push(
+          //     context,
+          //     MaterialPageRoute(builder: (context) => Salads()),
+          //   );
+          // },
+          child: Container(
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage(
+                    'assets/img/second_meal.jpg'),
+                fit: BoxFit.fitHeight,
+              ),
+              borderRadius: BorderRadius.circular(16),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade600,
+                  spreadRadius: 1,
+                  blurRadius: 5,
+                  offset: const Offset(0, 5),
+                )
+              ],
+            ),
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const [
+                Text(
+                  'Second Meal',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
           ),
-          ListTile(
-            leading:
-                Image.asset("assets/img/fish_meal.jpg", width: 70, height: 100),
-            title: Text('Fish meals'),
-            subtitle: Text('Кол-во предложений'),
-            onTap: () {},
+        ),
+
+
+        InkWell(
+          // onTap: () {
+          //   Navigator.push(
+          //     context,
+          //     MaterialPageRoute(builder: (context) => Salads()),
+          //   );
+          // },
+          child: Container(
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage(
+                    'assets/img/fish_meal.jpg'),
+                fit: BoxFit.fitHeight,
+              ),
+              borderRadius: BorderRadius.circular(16),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade600,
+                  spreadRadius: 1,
+                  blurRadius: 5,
+                  offset: const Offset(0, 5),
+                )
+              ],
+            ),
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const [
+                Text(
+                  'Fish Meal',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
           ),
-        ],
-      ),
+        ),
+
+
+        InkWell(
+          // onTap: () {
+          //   Navigator.push(
+          //     context,
+          //     MaterialPageRoute(builder: (context) => Salads()),
+          //   );
+          // },
+          child: Container(
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage(
+                    'assets/img/breakfast.jpg'),
+                fit: BoxFit.fitHeight,
+              ),
+              borderRadius: BorderRadius.circular(16),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade600,
+                  spreadRadius: 1,
+                  blurRadius: 5,
+                  offset: const Offset(0, 5),
+                )
+              ],
+            ),
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const [
+                Text(
+                  'Breakfast',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+
+      ],
+    ),
+
     );
   }
+
+//   Widget _buildGradient() {
+//     return Positioned.fill(
+//       child: DecoratedBox(
+//         decoration: BoxDecoration(
+//           gradient: LinearGradient(
+//             colors: [Colors.transparent, Colors.black.withOpacity(0.7)],
+//             begin: Alignment.topCenter,
+//             end: Alignment.bottomCenter,
+//             stops: const [0.6, 0.95],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
 }
